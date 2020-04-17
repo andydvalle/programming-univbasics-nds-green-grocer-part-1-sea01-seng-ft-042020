5b64834788882939126144e5c4206e8d9118859b
Andy Del Valle <andydelvalle89@gmail.com>
2020-04-17 17:00:44 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -21,10 +21,11 @@ def consolidate_cart(cart)
   cart.each do |hash|
     hash.each do |key,value|
     binding.pry
-      if find_item_by_name_in_collection(value,receipt)
+      if find_item_by_name_in_collection(value,cart)
         if hash[:count]
           hash[:count] +=1 
-        end
+        else
+          hash[:count] = 1
       end
     end
     #binding.pry
